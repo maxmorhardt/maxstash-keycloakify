cc9a5b7e6d505915b62d00fdfa2a55969ffd0af1
Max Morhardt <70174942+maxmorhardt@users.noreply.github.com>
2025-10-07 18:46:12 -0400

    info block mui

--- src/login/KcPage.tsx
+++ src/login/KcPage.tsx
@@ -4,7 +4,9 @@ import type { KcContext } from './KcContext';
 import { useI18n } from './i18n';
 import DefaultPage from 'keycloakify/login/DefaultPage';
 import Template from 'keycloakify/login/Template';
+
 const UserProfileFormFields = lazy(() => import('keycloakify/login/UserProfileFormFields'));
+const Login = lazy(() => import('./pages/Login'));
 
 const doMakeUserConfirmPassword = true;
 
@@ -17,6 +19,10 @@ export default function KcPage(props: { kcContext: KcContext }) {
     <Suspense>
       {(() => {
         switch (kcContext.pageId) {
+          case 'login.ftl':
+            return (
+              <Login {...{ kcContext, i18n, classes }} Template={Template} doUseDefaultCss={true} />
+            );
           default:
             return (
               <DefaultPage
--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,15 @@ import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
 import { KcPage } from './kc.gen';
 
-// The following block can be uncommented to test a specific page with `yarn dev`
-// Don't forget to comment back or your bundle size will increase
-/*
-import { getKcContextMock } from "./login/KcPageStory";
+// The following block can be uncommented to test a specific page with `npm run dev`
+// import { getKcContextMock } from './login/KcPageStory';
 
-if (import.meta.env.DEV) {
-    window.kcContext = getKcContextMock({
-        pageId: "register.ftl",
-        overrides: {}
-    });
-}
-*/
+// if (import.meta.env.DEV) {
+//   window.kcContext = getKcContextMock({
+//     pageId: 'login.ftl',
+//     overrides: {},
+//   });
+// }
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
