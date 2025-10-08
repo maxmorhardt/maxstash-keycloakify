a456e3560b11170a6b2af6e23e8aa17b6c3d1ff9
Max Morhardt <70174942+maxmorhardt@users.noreply.github.com>
2025-10-08 09:43:10 -0400

    maxstash logo

--- src/login/components/login/LoginTitle.tsx
+++ src/login/components/login/LoginTitle.tsx
@@ -1,5 +1,5 @@
-import { Typography } from '@mui/material';
-import { I18n } from 'keycloakify/login/i18n';
+import { Box, Typography } from "@mui/material";
+import { I18n } from "keycloakify/login/i18n";
 
 interface LoginTitleProps {
   i18n: I18n;
@@ -9,8 +9,24 @@ export default function LoginTitle({ i18n }: LoginTitleProps) {
   const { msg } = i18n;
 
   return (
-    <Typography variant="h5" textAlign="center" mb={3}>
-      {msg('loginAccountTitle')}
-    </Typography>
+    <>
+			<Box display={'flex'} justifyContent={'center'}>
+				<Box
+          component="img"
+          src="maxstash.png"
+          alt="maxstash"
+          sx={{
+            width: 40,
+            height: 40,
+            borderRadius: 1,
+            objectFit: "contain",
+          }}
+        />
+			</Box>
+
+      <Typography variant="h5" textAlign="center" mb={3} mt={2}>
+        {msg("loginAccountTitle")}
+      </Typography>
+    </>
   );
 }
