6dfdc5fef6ae95464073f1b4d9a636d0f657216d
Max Morhardt <70174942+maxmorhardt@users.noreply.github.com>
2025-10-08 17:58:37 -0400

    test fix

--- src/login/KcPage.tsx
+++ src/login/KcPage.tsx
@@ -30,9 +30,44 @@ export default function KcPage(props: { kcContext: KcContext }) {
           '.kcFormHeaderClass': { display: 'none !important' },
           '.kcHeaderClass': { display: 'none !important' },
 
-          'input:-webkit-autofill, input:-webkit-autofill:hover, input:-webkit-autofill:focus, textarea:-webkit-autofill, textarea:-webkit-autofill:hover, textarea:-webkit-autofill:focus, select:-webkit-autofill, select:-webkit-autofill:hover, select:-webkit-autofill:focus':
+          'input:-webkit-autofill, input:-webkit-autofill:hover, input:-webkit-autofill:focus, input:-internal-autofill-selected, textarea:-webkit-autofill, select:-webkit-autofill':
             {
               WebkitBoxShadow: '0 0 0 1000px transparent inset !important',
+              WebkitTextFillColor: '#ffffff !important',
+              caretColor: '#ffffff !important',
+              transition: 'background-color 9999s ease-in-out 0s !important',
+              borderColor: '#444 !important',
+              outline: 'none !important',
+            },
+
+          'input, textarea, select': {
+            color: '#ffffff !important',
+            backgroundColor: '#1e1e1e !important',
+            caretColor: '#ffffff !important',
+          },
+
+          'input:invalid, textarea:invalid, select:invalid': {
+            boxShadow: 'none !important',
+            borderColor: '#555 !important',
+            outline: 'none !important',
+          },
+
+          '.MuiOutlinedInput-root.Mui-error .MuiOutlinedInput-notchedOutline': {
+            borderColor: '#555 !important',
+          },
+          '.MuiFilledInput-root.Mui-error:after, .MuiInput-root.Mui-error:after': {
+            borderBottomColor: '#555 !important',
+          },
+        }}
+      />
+
+      <GlobalStyles
+        styles={{
+          '.kcFormHeaderClass': { display: 'none !important' },
+          '.kcHeaderClass': { display: 'none !important' },
+
+          'input:-webkit-autofill, input:-webkit-autofill:hover, input:-webkit-autofill:focus, textarea:-webkit-autofill, textarea:-webkit-autofill:hover, textarea:-webkit-autofill:focus, select:-webkit-autofill, select:-webkit-autofill:hover, select:-webkit-autofill:focus':
+            {
               WebkitTextFillColor: `${darkTheme.palette.text.primary} !important`,
               transition: 'background-color 9999s ease-in-out 0s !important',
               caretColor: `${darkTheme.palette.text.primary} !important`,
