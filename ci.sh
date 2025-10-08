d094b7aeb35e9a26b58d263f4bcc242722e687e1
Max Morhardt <70174942+maxmorhardt@users.noreply.github.com>
2025-10-08 00:45:52 -0400

    ci script

---
+++ ci.sh
@@ -0,0 +1,13 @@
+rm -rf dist_keycloak
+
+npm run build-keycloak-theme
+
+cd dist_keycloak
+unzip maxstash-keycloakify.jar
+cd ..
+
+docker buildx build --platform linux/arm64/v8 . \
+	-t maxmorhardt/maxstash-keycloakify:1.0.0 \
+	-t maxmorhardt/maxstash-keycloakify:latest
+
+docker push maxmorhardt/maxstash-keycloakify -a
